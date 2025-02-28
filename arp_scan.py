e1a9a962d4dfcafff58327b32f61de55c2420c32
Kamil Mróz <kamil.2003.kmz@gmail.com>
2025-02-28 17:33:29 +0100

    arp working for windows and linux

--- arp_scan.py
+++ arp_scan.py
@@ -1,8 +1,9 @@
 import subprocess
+import os
 import re
+import platform
 
-
-def get_ip_from_mac(mac_address):
+def get_ip_from_mac_windows(mac_address):
     try:
         # Run the arp command
         result = subprocess.run(['arp', '-a'], capture_output=True, text=True)
@@ -18,12 +19,33 @@ def get_ip_from_mac(mac_address):
 
     return None
 
+def get_ip_by_mac_linux(mac_address):
+    # Run the arp command to get a list of connected devices
+    output = os.popen("arp -a").read()
 
-# Example usage
-mac_address = "f4-91-1e-d3-c9-18"  # Ensure MAC format matches output (Windows uses '-')
-ip_address = get_ip_from_mac(mac_address)
+    # Regular expression to find IP and MAC address pairs
+    pattern = re.compile(r'\((.*?)\) at ([\w:]+)', re.IGNORECASE)
+
+    for match in pattern.findall(output):
+        ip, mac = match
+        if mac.lower() == mac_address.lower():
+            return ip
+    return None
+
+
+
+def get_ip_by_mac(mac_address):
+    os_name = platform.system()
+    if os_name == "Windows":
+        return get_ip_from_mac_windows(mac_address)
+    else:
+        return get_ip_by_mac_linux(mac_address)
+
+# Example MAC address (Replace with the actual MAC address)
+mac_address = "56:66:a3:d7:dd:22"
+ip_address = get_ip_by_mac(mac_address)
 
 if ip_address:
-    print(ip_address)  # Output only the IP address
+    print(f"IP Address for MAC {mac_address}: {ip_address}")
 else:
-    print("Device not found.")
+    print("MAC Address not found on the network")
